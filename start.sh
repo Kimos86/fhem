32f4140c3f86f6d4798360ca958ee307ba2dfd32
Kimos86 <kim.oliver.sitta@gmail.com>
2017-03-31 15:28:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # short workaround script
 
-/etc/init.d/dbus restart
+if [ ! -d /var/run/fhem  ]; then
+    mkdir /var/run/fhem
+    chown -R fhem:root /var/run/fhem
+fi
+
+#/etc/init.d/dbus restart
 #service avahi-daemon start
 /usr/bin/supervisord
