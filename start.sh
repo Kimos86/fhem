fe5aba71dc3b41103ee3dbf6ca1ba37e13276aca
Kimos86 <kim.oliver.sitta@gmail.com>
2017-04-01 01:57:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ ! -d /var/run/fhem  ]; then
     mkdir /var/run/fhem
     chown -R fhem:root /var/run/fhem
 fi
-
+chown -R fhem:root /opt/fhem
 #/etc/init.d/dbus restart
 #service avahi-daemon start
 /usr/bin/supervisord
