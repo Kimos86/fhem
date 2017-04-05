57803de2d1faaa2d435455af10b138436cf50299
Kimos86 <kim.oliver.sitta@gmail.com>
2017-04-05 11:41:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ if [ ! -d /var/run/fhem  ]; then
     mkdir /var/run/fhem
     chown -R fhem:root /var/run/fhem
 fi
+cp /opt/data/fhem.cfg /opt/fhem
 chown -R fhem:root /opt/fhem
 #/etc/init.d/dbus restart
 #service avahi-daemon start
